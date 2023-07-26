c2797781133e3e714679aa4891ade3f5e9c661bb
Manthan Singh Kushwah <99616527+Manthan-rajawat@users.noreply.github.com>
2023-07-26 17:17:16 +0530

    new changes

--- index.js
+++ index.js
@@ -237,6 +237,7 @@ require("dotenv").config();
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  userToken: process.env.SLACK_USER_TOKEN,
 });
 
 (async () => {
